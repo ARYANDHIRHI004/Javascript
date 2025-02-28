654c99f72bffab1f15242e38c65efedb2376f922
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-02-28 18:18:43 +0530

    Javascript variables

---
+++ Variable/main.js
@@ -0,0 +1,51 @@
+//String
+let Name = "Aryan"
+console.log(Name);
+console.log("Type of Name is ",typeof Name);
+
+
+//Number
+let age = 20
+console.log(age);
+console.log("Type of age is ",typeof age);
+
+//Boolean
+let isPaid = true
+console.log(isPaid);
+console.log("Type of isPaid is ",typeof isPaid);
+
+
+//null
+let fevouraiteClass = null;
+console.log(fevouraiteClass);
+console.log("Type of fevouraiteClass is ",typeof fevouraiteClass);
+
+
+//undefined
+let address 
+console.log(address);
+console.log("Type of address is ",typeof address);
+
+
+//Array
+let arr = [1,2,3,4,5,6]
+console.log(arr);
+console.log("Type of arr is ",typeof arr);
+
+
+//Object
+let obj = {
+    Name:"Aryan",
+    age:20,
+    skills:['java', 'C++', 'Javascript'],
+    address:{
+        hometown: undefined,
+        city: "Dabhara",
+        state: "Chhattisgarh",
+        country: "India"
+    }
+}
+console.log(obj);
+console.log("Type of obj is ",typeof obj);
+
+
